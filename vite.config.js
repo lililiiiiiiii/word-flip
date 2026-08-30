46cf380eb71c8ca9daf264d449c614f6a9661221
Lilylii <asmei418@yahoo.com.tw>
2026-08-30 20:14:48 +0800

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-vocab-app/', // ⚠️ 請改為你在 GitHub 上建的 Repository (倉庫) 名稱
+  base: '/word-flip/', // ⚠️ 注意：前後都必須有斜線 `/`！
 })
